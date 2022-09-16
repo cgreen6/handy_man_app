983d27d821461f188789bc5816678cd6e4f67b77
Christian Green <hoolio.suare@gmail.com>
2022-09-15 19:53:30 -0600

    filled out worker controller

--- app/controllers/api/workers_controller.rb
+++ app/controllers/api/workers_controller.rb
@@ -1,16 +1,43 @@
 class Api::WorkersController < ApplicationController
+
+  
   def index
+    render json: Worker.all
   end
 
   def show
+    @worker = Worker.find(params[:id])
+    render json: @worker
   end
 
   def create
+    @worker = Worker.new(worker_params)
+    if @worker.save
+    render json: @worker
+    else
+      render json: { errors: @worker.errors },
+    status: :unprocessable_entity
+    end
   end
 
   def update
+    @worker = Worker.find(params[:id])
+    if @worker.update(worker_params)
+      render json: @worker
+    else
+      render json: { errors: @worker.errors },
+      status: :unprocessable_entity
+    end
   end
 
   def destroy
-  end
+    @worker = Worker.find(params[:id])
+    @worker.destroy
+    render json: { message: 'worker deleted' }
 end
+
+private
+  def worker_params
+    params.require(:worker).permit(:title, :desc, :everything the table has)
+  end
+  
