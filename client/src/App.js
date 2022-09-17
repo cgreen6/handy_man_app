cde917241420e4f7400de0ff5b1d6adf93deb7c7
Christian Green <hoolio.suare@gmail.com>
2022-09-16 20:53:42 -0600

    added mainnavbar, nomatch.js, app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './components/shared/home/Home';
+import About from './components/shared/about/About';
+import Workers from './components/workers/Workers';
+import Nomatch from './components/shared/Nomatch';
+import MainNavbar from './components/shared/MainNavbar';
+import Comments from './components/comments/Comments';
 
+const App = () => (
+  <>
+    <MainNavbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/workers' element={<Lists />} />
+      <Route path='/:workerId/workers' element={<Worker />} />
+      <Route path='/:commentId/comments' element={<Comments />} />
+      <Route path='/*' element={<Nomatch />} />
+    </Routes>
+  </>
+)
 
 export default App;
