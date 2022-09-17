af024c1a03104ba40b34e61054cb10703e3a5cc3
Christian Green <hoolio.suare@gmail.com>
2022-09-16 22:37:37 -0600

    filled out services js pages

---
+++ client/src/components/services/ServiceList.js
@@ -0,0 +1,16 @@
+import WorkerShow from './WorkerShow';
+
+const ServiceList = ({ services, updateService, deleteService }) => (
+  <>
+    { services.map( n => 
+      <ServiceShow
+        key={n.id}
+        {...n}
+        updateService={updateService}
+        deleteService={deleteService}
+      />
+    )}
+  </>
+)
+
+export default ServiceList;
---
+++ client/src/components/services/Services.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import ServiceForm from './ServiceForm';
+
+const Services = () => {
+  const [services, setServices] = useState([])
+
+  useEffect( () => {
+    axios.get('/api/services')
+      .then( res => setServices(res.data))
+      .catch( err => console.log(err))
+  }, [])
+
+  const addService = (service) => {
+    axios.post('/api/services', { service })
+      .then( res => setServices([...services, res.data]))
+      .catch( err => console.log(err))
+  }
+
+  const updateService = (id, service) => {
+    axios.put(`/api/services/${id}`, { service })
+      .then( res => {
+        const newUpdatedServices = services.map( l => {
+          if (l.id == id) {
+            return res.data
+          }
+          return l
+        })
+        setServices(newUpdatedServices)
+      })
+      .catch( err => console.log(err))
+  }
+
+  const deleteService = (id) => {
+    axios.delete(`/api/services/${id}`)
+      .then( res => {
+        setServices(services.filter( l => l.id !== id))
+      })
+      .catch( err => console.log(err))
+  }
+
+  return (
+    <>
+      <ServiceForm addService={addService} />
+      <h1>All Services</h1>
+      <AllService 
+        services={services}
+        updateService={updateService}
+        deleteService={deleteService}
+      />
+    </>
+  )
+}
+
+export default Services;
